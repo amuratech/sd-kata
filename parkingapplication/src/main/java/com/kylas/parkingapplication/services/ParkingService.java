858d80c5d75774bbb9e2419a2e7bf9fd88488ce5
Akshay Gunshetti <akshay.gunshetti@kylas.io>
2020-09-18 16:37:11 +0530

    Using application.properties for reading values

--- parkingapplication/src/main/java/com/kylas/parkingapplication/services/ParkingService.java
+++ parkingapplication/src/main/java/com/kylas/parkingapplication/services/ParkingService.java
@@ -2,28 +2,33 @@ package com.kylas.parkingapplication.services;
 
 import com.kylas.parkingapplication.entities.Vehicle;
 import com.kylas.parkingapplication.exceptions.ParkingLotException;
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Service
 public class ParkingService {
+
+    @Value("${slot}")
+    private int parkingSlots;
 
     private List<Vehicle> vehicles=new ArrayList<>();
     static int parkingSlotNo=0;
 
-
-
-
     public List<Vehicle> getVehicles() {
+
         return vehicles;
     }
 
-    public Vehicle addVehicle(String vehicleNo) {
-        parkingSlotNo++;
 
-            if(parkingSlotNo<=3) {
+    public Vehicle addVehicle(String vehicleNo) {
+
+        parkingSlotNo++;
+        if(parkingSlotNo<=parkingSlots) {
                 vehicles.add(new Vehicle(parkingSlotNo,vehicleNo));
 
             }
@@ -35,4 +40,5 @@ public class ParkingService {
 
         return vehicles.get(vehicles.size()-1);
     }
+
 }
--- parkingapplication/src/test/java/com/kylas/parkingapplication/services/ParkingServiceTest.java
+++ parkingapplication/src/test/java/com/kylas/parkingapplication/services/ParkingServiceTest.java
@@ -1,32 +1,41 @@
 package com.kylas.parkingapplication.services;
 
 import com.kylas.parkingapplication.entities.Vehicle;
-import com.kylas.parkingapplication.exceptions.ParkingLotException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
 
 
 class ParkingServiceTest {
 
-
     @Test
-    public void givenCarNo_ShouldReturn() throws ParkingLotException {
+    public void givenCarNo_ShouldReturn() {
         ParkingService parkingService = new ParkingService();
+        ReflectionTestUtils.setField(
+                parkingService,
+                "parkingSlots",
+                3);
         Vehicle vehicle = parkingService.addVehicle("MH 14 LK 1234");
         Assertions.assertThat(vehicle.getVehicleNo()).isEqualTo("MH 14 LK 1234");
 
     }
 
     @Test
-    public void shouldReturnAllCars() throws ParkingLotException {
+    public void shouldReturnAllCars()  {
         ParkingService parkingService = new ParkingService();
+        ReflectionTestUtils.setField(
+                parkingService,
+                "parkingSlots",
+                3);
          parkingService.addVehicle("MH 14 LK 1234");
          parkingService.addVehicle("MH 14 LK 1235");
-        parkingService.addVehicle("MH 14 LK 1235");
         List<Vehicle> vehicles = parkingService.getVehicles();
-        Assertions.assertThat(vehicles.size()).isEqualTo(3);
+        Assertions.assertThat(vehicles.size()).isEqualTo(2);
 
     }
 
