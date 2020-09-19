a6917281095247a419c4a7a393f4b78603e19c3a
Akshay Gunshetti <akshay.gunshetti@kylas.io>
2020-09-19 10:57:58 +0530

    ParkingServiceTest.java reading slot value from application.properties

--- parkingapplication/src/test/java/com/kylas/parkingapplication/services/ParkingServiceTest.java
+++ parkingapplication/src/test/java/com/kylas/parkingapplication/services/ParkingServiceTest.java
@@ -4,17 +4,21 @@ import com.kylas.parkingapplication.entities.Vehicle;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
 
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = ParkingService.class)
 class ParkingServiceTest {
 
-
+    @Value("${slot}")
+    private int slots;
 
     @Test
     public void givenCarNo_ShouldReturn() {
@@ -22,7 +26,7 @@ class ParkingServiceTest {
         ReflectionTestUtils.setField(
                 parkingService,
                 "parkingSlots",
-                10);
+                slots);
         Vehicle vehicle = parkingService.addVehicle("MH 14 LK 1234");
         Assertions.assertThat(vehicle.getVehicleNo()).isEqualTo("MH 14 LK 1234");
 
@@ -34,12 +38,22 @@ class ParkingServiceTest {
         ReflectionTestUtils.setField(
                 parkingService,
                 "parkingSlots",
-                10);
+                slots);
          parkingService.addVehicle("MH 14 LK 1234");
          parkingService.addVehicle("MH 14 LK 1235");
         List<Vehicle> vehicles = parkingService.getVehicles();
         Assertions.assertThat(vehicles.size()).isEqualTo(2);
 
     }
+
+//    @Test
+//    public void shouldReturnExpectedExceptionType(){
+//        ParkingService parkingService = new ParkingService();
+//        ReflectionTestUtils.setField(
+//                parkingService,
+//                "parkingSlots",
+//                10);
+//       // Assertions.assertThrows()
+//    }
 
 }
